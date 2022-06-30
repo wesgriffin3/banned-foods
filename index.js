026a3e81840130de5c1d851dbeb6ad805df29bc1
Wes Griffin <WesGriffin@Wess-MacBook-Pro.local>
2022-06-29 22:08:15 -0500

    json responses

--- index.js
+++ index.js
@@ -1,22 +1,16 @@
-// fetch(
-//   "https://api.nutritionix.com/v1_1/search?appId=2b117551&appKey=060081dd5a36769c4ed8f2f30a178a47&query=" +
-// )
-//   .then((response) => response.json())
-//   .then((response) => console.log(response))
-//   .catch((err) => console.error(err));
-
-const input = $("input");
-const product = $("input").val();
+const form = $("form");
 
 form.submit(function () {
-    console.log(product);
-}
+  const product = $("#searchbar").val();
+  const productRequest = $.get(
+    "https://api.spoonacular.com/food/products/search?apiKey=cc0e26e2d9c44f39b29ebedb231e827b&query=" +
+      product +
+      "&number=5"
+  );
 
-// fetch(
-//   "https://api.spoonacular.com/food/products/search?apiKey=cc0e26e2d9c44f39b29ebedb231e827b&query=" +
-//     product,
-//   options
-// )
-//   .then((response) => response.json())
-//   .then((response) => console.log(response))
-//   .catch((err) => console.error(err));
+  productRequest.done(function (data) {
+    console.log(data.products);
+  });
+});
+
+const productResult = document.createElement("div");
