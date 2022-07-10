99b1863f9306d59dcd602ccd1df8071c9ee3f9a0
Wes Griffin <WesGriffin@Wess-MacBook-Pro.local>
2022-07-10 12:31:05 -0500

    api call displays to screen

--- index.js
+++ index.js
@@ -9,25 +9,34 @@ form.submit(function () {
   );
 
   productRequest.done(function (data) {
-    console.log(data.products);
+    console.log(data.products[0].title);
+    console.log(data.products[0].image);
+    console.log(data.products[0].ingredientList);
+    //
+    const title = document.getElementById("food-name");
+    title.innerHTML = data.products[0].title;
+    //
+    const image = document.getElementById("food-image");
+    image.src = data.products[0].image;
+    //
+    const ingredients = document.getElementById("ingredients");
+    ingredients.innerHTML = data.products[0].ingredientList;
   });
 });
 
-const productResult = document.createElement("div");
+// // scanner
 
-// scanner
+// var html5QrcodeScanner = new Html5QrcodeScanner("reader", {
+//   fps: 10,
+//   qrbox: 250,
+// });
 
-var html5QrcodeScanner = new Html5QrcodeScanner("reader", {
-  fps: 10,
-  qrbox: 250,
-});
+// function onScanSuccess(decodedText, decodedResult) {
+//   // Handle on success condition with the decoded text or result.
+//   console.log(`Scan result: ${decodedText}`, decodedResult);
+//   // ...
+//   html5QrcodeScanner.clear();
+//   // ^ this will stop the scanner (video feed) and clear the scan area.
+// }
 
-function onScanSuccess(decodedText, decodedResult) {
-  // Handle on success condition with the decoded text or result.
-  console.log(`Scan result: ${decodedText}`, decodedResult);
-  // ...
-  html5QrcodeScanner.clear();
-  // ^ this will stop the scanner (video feed) and clear the scan area.
-}
-
-html5QrcodeScanner.render(onScanSuccess);
+// html5QrcodeScanner.render(onScanSuccess);
